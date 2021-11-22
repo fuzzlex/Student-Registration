736b572fa38117c26347f24f54a2bfc72480c30b
emre <emrky.2018@icloud.com>
2021-11-22 03:01:33 +0300

    first commit

---
+++ student.js
@@ -0,0 +1,21 @@
+// class Student{
+//     constructor(a, b, c){
+//     this.a = a;
+//     this.b = b;
+//     this.c = c;
+//     }
+//     add(a,b){
+//     return a + b
+//     }
+//     k = 5;
+// }
+
+
+
+function Student(imageUrl, studentName, studentPath){
+    this.imageUrl = imageUrl;
+    this.studentName = studentName;
+    this.studentPath = studentPath;
+
+}
+
